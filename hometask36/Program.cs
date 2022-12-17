3f5c9559bb40311695fa0eec7a441f64f4a2e968
naumovanatalia <naumova.na43@mail.ru>
2022-12-17 14:48:00 +0200

    add hometask36

---
+++ hometask36/Program.cs
@@ -0,0 +1,22 @@
+﻿int [] GetRandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int [length];
+    Random rand = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+const int LENGTH = 12;
+const int LEFTRANGE = -50;
+const int RIGHTRANGE = 150;
+int [] massive = GetRandomArray(LENGTH, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine($"{string.Join(", ", massive)}");
+int sum = 0;
+for(int i = 1; i < massive.Length; i += 2 )
+{
+    sum += massive[i];
+}
+Console.WriteLine($"Сумма элементов, находящихся на нечетных позициях равна {sum}");
