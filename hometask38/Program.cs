85c6d99f111e5dddf579ffce0875070607ed361d
naumovanatalia <naumova.na43@mail.ru>
2022-12-17 15:01:50 +0200

    add hometask38

---
+++ hometask38/Program.cs
@@ -0,0 +1,31 @@
+﻿int [] GetRandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int [length];
+    Random rand = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+const int LENGTH = 7;
+const int LEFTRANGE = 1;
+const int RIGHTRANGE = 150;
+int [] massive = GetRandomArray(LENGTH, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine($"{string.Join(", ", massive)}");
+
+int min = massive[0];
+int max = massive[1];
+for(int i = 0; i < massive.Length; i++)
+{
+    if(massive[i] > max)
+    {
+        max = massive[i];
+    }
+    else if(massive[i] < min)
+    {
+        min = massive[i];
+    }
+}
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {max -min}");
