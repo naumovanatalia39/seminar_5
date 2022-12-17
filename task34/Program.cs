ebe9259155e61a3f53af23998b1fdd0cf36f5d89
naumovanatalia <naumova.na43@mail.ru>
2022-12-17 14:14:06 +0200

    add hometask34

---
+++ task34/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = new int [9];
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = new Random().Next(100,1000);
+}
+Console.Write(string.Join(", ", array));
+Console.WriteLine();
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+        count ++;
+    }
+}
+Console.WriteLine($"Количество чётных чисел в массиве {count}");
