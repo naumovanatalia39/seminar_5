1cb23f6b41016cd11e940de3afac2843318341e7
naumovanatalia <naumova.na43@mail.ru>
2022-12-17 13:59:19 +0200

    add task 37

---
+++ task37/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = {1,2,3,4,5};
+int count = 1;
+if(array.Length % 2 == 0)
+{
+    count = array.Length / 2;
+}
+else
+{
+    count = array.Length / 2 + 1;
+}
+int[] array2 = new int [count]; // int[] array2 = new int[array.Length / 2 + array.Length % 2]
+
+for(int i = 0; i < array2.Length; i++)
+{
+    array2[i] = array[i] * array[array.Length - 1 - i];
+    if(i == array.Length -i -1)
+    {
+        array2[i] = array[i];
+    }
+}
+Console.WriteLine(string.Join(", ",array2));
